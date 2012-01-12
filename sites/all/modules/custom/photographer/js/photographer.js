d5d743ead846e06f7569e968fe8cd402fca5bd62
unknown <yogaraj@syblap6.sybserver.com>
2012-01-12 18:49:43 +0530

    moving custom functionalities to module custom

---
+++ sites/all/modules/custom/photographer/js/photographer.js
@@ -0,0 +1,50 @@
+jQuery(document).ready(function()
+{
+	
+	document.getElementById('selected_event_drag').style.display='none';
+
+
+});
+
+/**
+ * This function is used for show and hide the upload section when without selecting the event
+ */
+function select_event_upload()
+{
+	var selected_event=document.getElementById('event_select').value;
+
+
+	//For get the selected  value from select box to the other zip file upload form hidded text field.because of two form in the single page.so we want to pass the selected event name from drag and drop upload form to zip file upload form
+
+	if(selected_event!='select_event')
+	{
+		document.getElementById('selected_event_drag').value= selected_event;
+		document.getElementById('upload_section').style.display= 'block';
+	}
+	else
+	{
+
+		document.getElementById('upload_section').style.display= 'none';
+
+	}
+	
+}
+
+/**
+ * Drag and drop upload validation
+ */
+function drag_drop_upload_validation()
+{
+	var selected_value=document.getElementById('event_select').value;
+
+	if(selected_value=='select_event')
+	{
+		alert('Select the event');
+		return false;
+	
+	}
+
+
+
+}
+
