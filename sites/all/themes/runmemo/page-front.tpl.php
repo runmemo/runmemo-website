6fadda11a2dfac433af0aba24c1c0a393f8d1038
unknown <yogaraj@syblap6.sybserver.com>
2012-01-13 19:27:40 +0530

    theme integration

---
+++ sites/all/themes/runmemo/page-front.tpl.php
@@ -0,0 +1,84 @@
+<link href="<?php echo base_path().path_to_theme(); ?>/css/style.css" type="text/css" rel="stylesheet" />
+<style type="text/css">
+@font-face {
+	font-family:'Futura-LT-Light';
+	src: url('Futura-LT-Light.ttf');
+	src: url('Futura-LT-Light.otf');
+	font-weight: normal;
+	font-style: normal;
+}
+@font-face {
+	font-family: 'Walkway Bold';
+	src: url('Walkway Bold.ttf');
+	font-weight: normal;
+	font-style: normal;
+}
+</style>
+
+<?php
+?>
+  <?php print render($page['header']); ?>
+
+  <div id="wrapper">
+    <div id="container" class="clearfix">
+
+      <div id="header">
+        <div id="logo-floater">
+        <?php if ($logo || $site_title): ?>
+          <?php if ($title): ?>
+            <div id="branding"><strong><a href="<?php print $front_page ?>">
+            <?php if ($logo): ?>
+              <img src="<?php print $logo ?>" id="logo" />
+            <?php endif; ?>
+            
+            </a></strong></div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="branding"><a href="<?php print $front_page ?>">
+            <?php if ($logo): ?>
+              <img src="<?php print $logo ?>" id="logo" />
+            <?php endif; ?>
+            
+            </a></h1>
+        <?php endif; ?>
+        <?php endif; ?>
+        </div>
+
+        
+      </div> <!-- /#header -->
+
+      <?php if ($page['sidebar_first']): ?>
+        <div id="sidebar-first" class="sidebar">
+          <?php print render($page['sidebar_first']); ?>
+        </div>
+      <?php endif; ?>
+
+      <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">
+          <?php print $breadcrumb; ?>
+          <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+          <a id="main-content"></a>
+          <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?>
+            <h1<?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
+          <?php print render($tabs2); ?>
+          <?php print $messages; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          <div class="clearfix">
+            <?php print render($page['content']); ?>
+          </div>
+          <?php print $feed_icons ?>
+          <?php print render($page['footer']); ?>
+      </div></div></div></div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+
+      <?php if ($page['sidebar_second']): ?>
+        <div id="sidebar-second" class="sidebar">
+          <?php print render($page['sidebar_second']); ?>
+        </div>
+      <?php endif; ?>
+
+    </div> <!-- /#container -->
+  </div> <!-- /#wrapper -->
