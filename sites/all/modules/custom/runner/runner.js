8e033f83b09c0be3a9b992220276f6a5afec4942
unknown <yogaraj@syblap6.sybserver.com>
2012-02-08 20:18:35 +0530

    get started work

--- sites/all/modules/custom/runner/runner.js
+++ sites/all/modules/custom/runner/runner.js
@@ -1,33 +1,36 @@
-jQuery(document).ready(function()
-{
+jQuery(document).ready(function(){
 	
 
 });
 
-//onclick function for in the my number text field to hide the default text
-function my_number_hide()
-{
-	document.getElementById('runner_number').value='';	
+/**
+ * onclick function for in the my number text field to hide the default text
+ */
+function my_number_hide(){
+    document.getElementById('runner_number').value='';	
 }
-function my_number_show()
-{
-	document.getElementById('runner_number').value='My Number';	
-}
-
-//for sell the photos in the runner side
-function select_event_runner_sell()
-{
-
-
+function my_number_show(){
+    document.getElementById('runner_number').value='My Number';	
 }
 
+/**
+ * validation for search button field in the home 
+ */
+function search_validation(){
+    var event_name_search=document.getElementById('event_runner').value;
+    if(event_name_search=='select_event'){
+        return false;
+    }
 
-//validation for search button field in the home 
-function search_validation()
-{
-
-	var event_name_search=document.getElementById('event_runner').value;
-	if(event_name_search=='select_event')
-		return false;
 }
 
+/**
+ * Get started button validation in the home page
+ */
+function get_started_validation(){
+    var event_name_search_id=document.getElementById('event_runner_sell').value;
+    if(event_name_search_id=='select_event'){
+         return false;
+    }
+           
+}
