7e5c0f04b18ccc723930fb7a3b4dbdb6372d6588
unknown <yogaraj@syblap6.sybserver.com>
2012-02-10 21:03:20 +0530

    add regions

--- sites/all/modules/custom/runner/runner.js
+++ sites/all/modules/custom/runner/runner.js
@@ -17,9 +17,10 @@ function my_number_show(){
  * validation for search button field in the home 
  */
 function search_validation(){
-    var event_name_search=document.getElementById('event_runner').value;
-    if(event_name_search=='select_event'){
-        return false;
+    var event_name_search = document.getElementById('event_runner').value;
+    if(event_name_search == 'select_event'){
+      alert("Please select Event");
+      return false;
     }
 
 }
@@ -28,9 +29,21 @@ function search_validation(){
  * Get started button validation in the home page
  */
 function get_started_validation(){
-    var event_name_search_id=document.getElementById('event_runner_sell').value;
-    if(event_name_search_id=='select_event'){
-         return false;
+    var event_name_search_id = document.getElementById('event_runner_sell').value;
+    if(event_name_search_id == 'select_event'){
+      alert("Please select Event");
+      return false;
     }
            
+}
+
+/**
+ * Search using runner number in the recent event section
+ */
+function find_recent_event1(){
+  var recent_event1_number = document.getElementById('number_recent_event1').value;
+  if(recent_event1_number == ''){
+      alert("Please enter your number");
+      return false;
+    }
 }
