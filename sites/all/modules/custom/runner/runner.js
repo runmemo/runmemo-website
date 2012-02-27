71ba801e60cf1587335028d211baed2e8ddeb4de
unknown <Yogaraj@sybwks168.sybserver.com>
2012-02-27 22:12:56 +0530

    feedback functionality

--- sites/all/modules/custom/runner/runner.js
+++ sites/all/modules/custom/runner/runner.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function(){
-	
-
+      
 });
 
 /**
@@ -50,93 +49,97 @@ function search_result_add_to_cart() {
  */
 (function ($) {
      
-
-  
   Drupal.behaviors.runmemo = { 
        
      attach: function (context,settings) {
     
-       $('#block-system-main table.views-view-grid td').each(function(){
-     var markup = $(this).html();
+      $('#block-system-main table.views-view-grid td').each(function(){
+              var markup = $(this).html();
+
+
+
+              if ($(this).has("div").length == 0) {
+                      $(this).css("border", "none");
+              }
+
+      });
+      if ($('#block-system-main table.views-view-grid td.col-first img').length == 1) {
+              var initial_img = $('#block-system-main table.views-view-grid td.col2 img').attr('src');	
+              var initial_preview=initial_img.replace('thumbnail', 'preview-with-watermark');
+              /*alert(replacementurl);*/
+              $("#preview_image").attr("src",initial_preview);
+
+      }
+
+
+
+      $('div.node_check').each(function(){
+              var name_id = $(this).parent().children('span').text();
+              $(this).html('<input type="checkbox" name="'+name_id+'" value="'+name_id+'" />');
+      });
+      $('#block-system-main div.field-content img').bind('click', function(){ 
+              var imgsrc = $(this).attr('src');	
+              var replacementurl = imgsrc.replace('thumbnail', 'preview-with-watermark');
+
+              //alert(replacementurl);
+              $("#preview_image").attr("src",replacementurl);
+
+
+      });	
+    
+    
+   
+    if (($("#block-feedback-form")).length > 0 ) {
+        
+        $("#block-feedback-form .content").hide();
+        
+      }
+    
+     Drupal.feedbackFormToggle = function ($block, enable) {
+      $block.find('form').slideToggle('medium');
+      if (enable) {
+        $('#feedback-form-toggle', $block).html('[ + ]');
+        $("#block-feedback-form .content").hide();
+         $("#feedback-form").attr('style','display:none;');
+        
+      }
+      else {
+        $('#feedback-form-toggle', $block).html('[ &minus; ]');
+        $("#block-feedback-form .content").show();
+         $("#feedback-form").attr('style','display:block;');
+         
+      }
+    };
+    
+     if($(".error").length > 0 ) {
+         
+         $("#block-feedback-form .content").show();
+         $("#feedback-form").attr('style','display:block;');
+         
+     }
+         
+   
+   
+   $("#fb_close_link").click(function() {
      
-     if ($(this).has("div").length == 0) {
-      $(this).css("border", "none");
+      $("#feedback-form-toggle").trigger('click');
+        
+      if($(".error").length > 0 ) {
+       $("#feedback-form-toggle").trigger('');
+       
+     }
+     else {
+       $("#feedback-form-toggle").trigger('');
      }
      
-    });
-   
-    $('#block-system-main div.field-content img').bind('click', function(){ 
-     var imgsrc = $(this).attr('src'); 
-     var replacementurl = imgsrc.replace('thumbnail', 'preview-with-watermark');
-     
-     //alert(replacementurl);
-     $("#preview_image").attr("src",replacementurl);
- 
-                         
-    }); 
-   
-   
-     $("#feedback-form").prepend("<a id='feedback_close'>Close</a>");
-     $("#feedback_close").css('cursor', 'pointer');
-      
-     $(".feedback-link").html('');
-     $("#block-feedback-form h2").click(function() {
-
-      $("#block-feedback-form .content").show();
-      $("#feedback-form").show();
      });
-     $("#feedback_close").click(function() {
-         $("#block-feedback-form .content").hide();
-         $("#feedback-form").attr('style','display:none;');
-     });
-     
-     if (($("#block-feedback-form")).length > 0 ) {
-        
-        $("#block-feedback-form .content").hide();
-        
-      }
-    
-    /* 
-    //for feedback content display
-      if (($("#block-feedback-form")).length > 0 ) {
-       
-        $("#block-feedback-form .content").hide();
-        //$("#block-feedback-form h2 span").hide();
-        //$(".feedback-link").hide() ;
-      }
-      /**
-      * Collapse or uncollapse the feedback form block.
-      */
-     
-     /*
-      Drupal.feedbackFormToggle = function ($block, enable) {
-         
-        $block.find('form').slideToggle('medium');
-        if (enable) {
-          $('#feedback-form-toggle', $block).html('');
-          $("#block-feedback-form .content").hide();
-          
-        }
-        else {
-          $('#feedback-form-toggle', $block).html('');
-          $("#block-feedback-form .content").show();
-        }
-      };
-   
-      $("#feedback-form").prepend("<a id='feedback_close'>Close</a>");
-      $("#feedback_close").css('cursor', 'pointer');
-     
-     $("#feedback_close").click(function() {
-      $("#block-feedback-form .content").hide();
-     });
-    
-   */
-   
   }
 
 
 
   };
+  
+  
   
      
 })(jQuery);
