5bb8186aa25810ac958d92feea38d7864a8fd79e
unknown <Yogaraj@sybwks168.sybserver.com>
2012-03-06 21:30:16 +0530

    search page check box

--- sites/all/modules/custom/runner/runner.js
+++ sites/all/modules/custom/runner/runner.js
@@ -48,7 +48,7 @@ function search_result_add_to_cart() {
 
       $('div.node_check').each(function(){
               var name_id = $(this).parent().children('span').text();
-              $(this).html('<input type="checkbox" name="'+name_id+'" value="'+name_id+'" />');
+              $(this).html('<label class="label_check" for="'+name_id+'"><input type="checkbox" name="'+name_id+'" value="'+name_id+'" /></label>');
       });
       $('#block-system-main div.field-content img').bind('click', function(){ 
               var imgsrc = $(this).attr('src');	
@@ -70,11 +70,11 @@ function search_result_add_to_cart() {
     if($(".page-search-result .error ").length > 0 ) {
         if($(".page-search-result .error ul li").length > 0 ) {
           $(".page-search-result #sidebar-second .block .content").attr('style','top:92px');
-          $(".page-search-result .footer").attr('style','margin-top:80px');
+          $(".page-search-result .left-corner").attr('style',' min-height: 974px;');
         }
         else {
           $(".page-search-result #sidebar-second .block .content").attr('style','top:62px');
-           $(".page-search-result .footer").attr('style','margin-top:48px');
+           $(".page-search-result .left-corner").attr('style',' min-height: 944px;');
         }
             
      }
@@ -85,9 +85,11 @@ function search_result_add_to_cart() {
         $("#block-feedback-form .content").hide();
         $("body.page-feedback #block-feedback-form .content").show();
 		$("body.page-feedback #block-feedback-form #feedback-form").show();
+		var txt_link = parent.window.document.location;
+		$("input#edit-url-hidden").val(txt_link);
       }
-	  
-    $("#block-feedback-form h2").html('<a class="feedback-link-new" href="feedback">link</a>');
+	 var link_chg = $("div#link-display").text();
+    $("#block-feedback-form h2").html('<a class="feedback-link-new" href="'+link_chg+'">link</a>');
 	
      Drupal.feedbackFormToggle = function ($block, enable) {
       $block.find('form').slideToggle('medium');
@@ -131,19 +133,86 @@ function search_result_add_to_cart() {
      }*/
      
      });
+	 
+/**
+ * Re-collapse the feedback form after every successful form submission.
+ */
+Drupal.behaviors.feedbackFormSubmit = {
+  attach: function (context) {
+   
+    var $context = $(context);
+    if (!$context.is('#feedback-status-message')) {
+      return;
+    }
+    // Collapse the form.
+    $('#block-feedback-form .feedback-link').click();
+    // Blend out and remove status message.
+    window.setTimeout(function () {
+      $context.fadeOut('slow', function () {
+        $context.remove();
+      });
+    }, 3000);
 	
-	Drupal.behaviors.feedbackFormSubmit = function (context) {
-		parent.Drupal.overlay.close();
-	};
-	
-	
+	parent.Drupal.overlay.close();
   }
-
-
-
-  };
+};
+}
+//onload ends here..
+};
   
   
+  /*Check box design for search results page*/
   
+  var d = document;
+    var safari = (navigator.userAgent.toLowerCase().indexOf('safari') != -1) ? true : false;
+    var gebtn = function(parEl,child) { return parEl.getElementsByTagName(child); };
+    onload = function() {
+        
+        var body = gebtn(d,'body')[0];
+        body.className = body.className && body.className != '' ? body.className + ' has-js' : 'has-js';
+        
+        if (!d.getElementById || !d.createTextNode) return;
+        var ls = gebtn(d,'label');
+        for (var i = 0; i < ls.length; i++) {
+            var l = ls[i];
+            if (l.className.indexOf('label_') == -1) continue;
+            var inp = gebtn(l,'input')[0];
+            if (l.className == 'label_check') {
+                l.className = (safari && inp.checked == true || inp.checked) ? 'label_check c_on' : 'label_check c_off';
+                l.onclick = check_it;
+            };
+            if (l.className == 'label_radio') {
+                l.className = (safari && inp.checked == true || inp.checked) ? 'label_radio r_on' : 'label_radio r_off';
+                l.onclick = turn_radio;
+            };
+        };
+    };
+    var check_it = function() {
+        var inp = gebtn(this,'input')[0];
+        if (this.className == 'label_check c_off' || (!safari && inp.checked)) {
+            this.className = 'label_check c_on';
+            if (safari) inp.click();
+        } else {
+            this.className = 'label_check c_off';
+            if (safari) inp.click();
+        };
+    };
+    var turn_radio = function() {
+        var inp = gebtn(this,'input')[0];
+        if (this.className == 'label_radio r_off' || inp.checked) {
+            var ls = gebtn(this.parentNode,'label');
+            for (var i = 0; i < ls.length; i++) {
+                var l = ls[i];
+                if (l.className.indexOf('label_radio') == -1)  continue;
+                l.className = 'label_radio r_off';
+            };
+            this.className = 'label_radio r_on';
+            if (safari) inp.click();
+        } else {
+            this.className = 'label_radio r_off';
+            if (safari) inp.click();
+        };
+    };
+  /*End Check box design for search results page*/
      
 })(jQuery);
