084e42deb26bf19fb8b8c69869764a89ab15dacf
Sowmya Sreenivasan <sowmya.s@sybrant.com>
2012-04-17 15:42:36 +0530

    feedback form changes

--- sites/all/themes/runmemo/template.php
+++ sites/all/themes/runmemo/template.php
@@ -165,13 +165,15 @@ function runmemo_preprocess_region(&$vars) {
 * Implementation of HOOK_theme().
 */
 function runmemo_theme($existing, $type, $theme, $path) {
+  
   return array( 
-	'uc_cart_checkout_form' => array(
-		'render element' => 'form',
-		'template' => 'uc-cart-checkout-form',
-		'path' => drupal_get_path('theme', 'runmemo'),
-		),
-  );
+            'uc_cart_checkout_form' => array(
+                    'render element' => 'form',
+                    'template' => 'uc-cart-checkout-form',
+                    'path' => drupal_get_path('theme', 'runmemo'),
+                    ),
+            
+          );
 }
 
 
@@ -179,21 +181,17 @@ function runmemo_theme($existing, $type, $theme, $path) {
  * Override checkout page design.
  */
 function runmemo_preprocess_uc_cart_checkout_form(&$variables) {
-  //echo '<pre>';
-  //print_r($variables['form']['actions']);
-  //exit;
-  
+ 
   $form_build_id = $variables['form']['form_build_id'];
-  $form_token  =  $variables['form']['form_token'] ;
+  //$form_token  =  $variables['form']['form_token'] ;
   $form_id = $variables['form']['form_id'];
-   $variables['form_build_id'] = drupal_render($form_build_id);
+  $variables['form_build_id'] = drupal_render($form_build_id);
   $variables['form_token'] = drupal_render($form_token);
   $variables['form_id'] = drupal_render($form_id);
   
   $billing = $variables['form']['panes']['billing'];
   $customer = $variables['form']['panes']['customer'];
   $payment = $variables['form']['panes']['payment'];
-  //$cart = $variables['form']['panes']['cart']['cart_review_table']['#items'];
   $submit = $variables['form']['actions'];
   $form_build_id = $variables['form']['form_build_id'];
   
@@ -201,18 +199,109 @@ function runmemo_preprocess_uc_cart_checkout_form(&$variables) {
   $variables['email'] = $customer['#theme']['primary_email'];
   $variables['customer'] = drupal_render($customer);
   $variables['payment']  = drupal_render_children($payment);
-  //$variables['cart']     = $cart;
-  //$variables['product_image'] = $cart_details;
   $variables['submit'] = drupal_render($submit);
   $variables['form_build_id'] = drupal_render($form_build_id);
  
 }
-/*
+
 // Default function from uc_cart.module.
-function theme_uc_cart_checkout_review($help, $panes, $form) {
-  echo '<pre>';
-  print_r($panes);
-  exit;
+function runmemo_preprocess_uc_cart_checkout_review(&$variables) {
+  
+  $cart_items = uc_cart_get_contents();
+  $no_of_cart_items = count($cart_items);
+  foreach($cart_items as $product => $value){
+ 
+    $photo[] = $value->uc_product_image['und'][0];
+    
+    //for cost format in the review order page mouse over image 
+    $cost = $value->sell_price;
+    $cost = number_format($cost, 2, '.', '');
+    $cost_arr = explode('.', $cost);
+    if($cost_arr['1'] == 00) {
+      $price[] = $cost_arr['0'];
+    }
+    else {
+      $price[] = $cost;
+    }
+  }
+ 
+  $output = '<table class="review-order-img" ><tr>';
+  $j = 0;
+  for($i=0;$i<$no_of_cart_items;$i++) {
+    $j++;
+    $output .=  '<td>'. '<div class="views-field-sell-price"><span id="node_cost" style="display:none">£'. $price[$i] . '</span></div>'  .theme("image_formatter", array('item' => $photo[$i], 'image_style' => 'thumbnail'));
+    $output .= '</td>';
+      
+    if($j%4 == 0) {
+     $output .= '</tr><tr>';
+    }
+  }
+  
+  if($no_of_cart_items < 4) {
+      for($k=0;$k<(4-$no_of_cart_items);$k++){
+        $output .= '<td></td>';
+      }
+  }
+  
+  $output .= '</tr></table>';
+  $variables['panes']['Cart contents']['0'] =  $output;
+  
+  return $variables;
+  
 }
- * 
+ 
+/**
+ * Themes the sale completion page.
+ *
+ * @param $variables
+ *   
+ * @ingroup themeable
  */
+
+function runmemo_preprocess_uc_cart_complete_sale(&$variables) { 
+ 
+ foreach($variables['order']->products as $order=>$image){
+    $nid = $image->nid;
+    $node = node_load($nid);
+    
+    $img_uri[] = $node->uc_product_image['und'][0];
+    $img_name[] = $node->uc_product_image['und'][0]['filename'];
+  }
+  
+  
+  $no_of_image = count($img_uri);
+  
+  $output = '<div class="order-complete-title">Your payment is complete,thank you for your order</div><div class="order-complete-preview"><h1 class="float-L">Preview Image</h1>';
+  $output .= '<div class="order-sale-description">Click on the preview to open original size image in the new window</div>'; 
+  $output .= '<table><tr>';
+  $j = 0;
+  for($i=0;$i<$no_of_image;$i++) {
+    
+    $resource = $img_name[$i];
+    if(module_exists('s3_store') && function_exists('get_s3_signed_url')){
+      $original_img_url = get_s3_signed_url($resource);
+    }
+    
+    $j++;
+    $output .=  '<td><a href="' . $original_img_url .'" target="_blank">' . theme("image_formatter", array('item' => $img_uri[$i], 'image_style' => 'thumbnail')) .'</a>';
+    $output .= '</td>';
+    
+        
+    if($j%4 == 0) {
+     $output .= '</tr><tr>';
+    }
+  }
+  
+  if($no_of_image < 4) {
+      for($k=0;$k<(4-$no_of_image);$k++){
+        $output .= '<td></td>';
+      }
+  }
+  
+  $output .= '</tr></table><div style="clear:both;"></div></div>';
+  
+  $variables['message'] =  $output;
+  
+  return $variables;
+}
+ 
