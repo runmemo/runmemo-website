009541a2d744a8c073764abe22df22d67d1d7425
byapparov <byapparov@gmail.com>
2012-07-09 01:57:07 +0100

    Removed unnessary code from Feedback Custom module.

--- sites/all/modules/custom/feedback_custom/feedback_custom.js
+++ sites/all/modules/custom/feedback_custom/feedback_custom.js
@@ -3,52 +3,17 @@
  */
 (function($) {
 
-Drupal.behaviors.Runmemo = {
-	attach: function (context) {
-		if (($("#block-feedback-form")).length > 0) {
-
-			$("#block-feedback-form .content").hide();
-			$("body.page-feedback #block-feedback-form .content").show();
-			$("body.page-feedback #block-feedback-form #feedback-form").show();
-			
-			var txt_link = parent.window.document.location;
-			$("input#edit-url-hidden").val(txt_link);
+	Drupal.behaviors.feedbackFormCustom = {
+		attach: function (context) {
+			/**
+			 * Close overlay on close button click
+			 */
+			$("#fb_close_link").click(function() {
+				$('#block-feedback-form .feedback-link').click();
+			});
 		}
-		
-		var link_chg = $("div#link-display").text();
-		$("#block-feedback-form h2").html('<a class="feedback-link-new" href="' + link_chg + '"></a>');
+	};
 
-		if ($("#feedback-form .error").length > 0) {
-
-			$("#block-feedback-form .content").show();
-			$("#feedback-form").attr('style', 'display:block;');
-
-		}
-
-		/**
-		 * Close overlay on close button click
-		 */
-		$("#fb_close_link").click(function() {
-
-			parent.Drupal.overlay.close();
-
-		});
-	}
-};
-/**
- * Hide Overlay.
- */
-Drupal.behaviors.feedbackFormSubmit = {
-  attach: function (context) {
-	  var $context = $(context);
-	    if (!$context.is('#feedback-status-message')) {
-	      return;
-	    }
-	  parent.Drupal.overlay.close();
-  }
-
-};
-	
 })(jQuery);
 			
 
