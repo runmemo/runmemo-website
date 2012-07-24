f8b7e3b1b3e813cae1697edada2327a17b792346
byapparov <byapparov@gmail.com>
2012-07-24 08:05:44 +0100

    Merge changes

--- sites/all/themes/runmemo/js/home_page.js
+++ sites/all/themes/runmemo/js/home_page.js
@@ -2,6 +2,12 @@ jQuery(document).ready(function($) {
 	$('.front #runner_number').val('My Number');
 	$('.recent-event .form-text').val('My Number');
 	
+	
+	$('#user-login #edit-name').val('Username');
+	
+	$('#edit-pass').hide();
+	
+	
 	// for remove the set message when adding items in the cart
 	if ($('.messages a').length > 0) {
 
@@ -12,13 +18,14 @@ jQuery(document).ready(function($) {
 	
 });
 
+
 /**
  * jQuery Behaviors for home page elements
  */
 (function($) {
 	Drupal.behaviors.runmemoHomePage = {
 		attach : function(context, settings) {
-			
+		
 			// runner number in the find section for front page
 			$('.front #runner_number').focus( function() {
 				if ($(this).val() == 'My Number') {
@@ -30,12 +37,40 @@ jQuery(document).ready(function($) {
 					$(this).val('My Number');
 				}
 			});
-
+	
 			$('.front #runner_number').change( function() {
 				if ($(this).val() == '') {
 					$(this).val('My Number');
 				}
 			});
+			
+			$('#user-login #edit-name').focus( function() {
+				if ($(this).val() == 'Username') {
+					$(this).val("");
+				}
+			});
+		
+			$('#user-login #edit-name').blur( function() {
+				if ($(this).val() == '') {
+					$(this).val('Username');
+				}
+			});
+		
+			$('#edit-pass').blur( function() {
+			    if ($(this).attr('value') == '') {
+			        $(this).hide();
+			        $('#edit-pass-fake').show();
+			    }
+				
+			});
+			
+			$('#edit-pass-fake').focus( function() {
+				 $(this).hide();
+				 $('#edit-pass').show();
+				 $('#edit-pass').focus();
+			});
+		
+			
 			// recent event 'My Number' text
 			$('.recent-event .form-text').focus( function() {
 
@@ -43,6 +78,7 @@ jQuery(document).ready(function($) {
 					$(this).val("");
 				}
 			});
+			// on recent event blur
 			$('.recent-event .form-text').blur( function() {
 
 				if ($(this).val() == '') {
