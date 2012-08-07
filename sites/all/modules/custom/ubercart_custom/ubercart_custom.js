aa20b236ed50e78c4d914fd3d5989404a94f22d2
byapparov <byapparov@gmail.com>
2012-08-07 15:17:45 +0100

    Feature: added remove button to checkout page.

--- sites/all/modules/custom/ubercart_custom/ubercart_custom.js
+++ sites/all/modules/custom/ubercart_custom/ubercart_custom.js
@@ -93,7 +93,7 @@
 	        url : base_path + "ajax/change_price",
 	        type: "POST",
 	        data: {nid: nid, price: price},
-                dataType: 'json',
+	        dataType: 'json',
 	        cache: false,
 	        success : function(msg) {              
                   if (msg.success) {
@@ -121,4 +121,57 @@
 	
 		}
 	};
+	
+	Drupal.behaviors.runmemoCheckoutPage = {
+			attach : function(context, settings) {
+				
+				$('.remove-cart-item').click(function(event){
+					var nid = $(this).attr('id');
+	  				
+	  				hide_item(nid);
+	  				remove_from_cart(nid);
+	  				
+				
+				});
+				/**
+				 * Removes item from cart and updates order summary on success
+				 * 
+				 * @param nid - Node id of the item that will be removed from cart
+				 */
+				function remove_from_cart(nid) {
+					var base_path = Drupal.settings.basePath;
+					// remove item from ubercart
+					$.ajax({
+						// type : "POST",
+						url : base_path + "cart_remove_item?nid=" + nid,
+						//dataType: 'json',
+						success : function(msg) {
+							set_checkout_summary(msg);
+						}
+					});
+				}
+				
+				/**
+				 * Sets page elements to values from ajax call response
+				 * @param response_json
+				 */
+				function set_checkout_summary(response_json) {
+					var response = jQuery.parseJSON(response_json)
+					if (response.success) {
+						$('#order-subtotal').html(response.subtotal);
+						$('#order-items').html(response.items);
+					}
+					else {
+						console.debug('Ajax call failed :' + response.message)
+					}
+				}
+				
+				function hide_item(nid) {
+					console.debug('Removing div - ' + '#thumb-' + nid);
+					$('#thumb-' + nid).remove();					
+				}
+				
+			}
+	}
+				
 })(jQuery);
--- sites/all/themes/runmemo/uc-cart-checkout-form.tpl.php
+++ sites/all/themes/runmemo/uc-cart-checkout-form.tpl.php
@@ -1,81 +1,43 @@
-
 <div class="checkoutPane">
-	<div class="paymentPane"><div><span class="checkout_title">Payment Details</span></div>
-	<?php print $billing;
-              print $customer;
-              print $payment;
-              print $form_build_id;
-              print $form_token;
-              print $form_id;
-              print $submit;
+	<div class="paymentPane">
+		<div><span class="checkout_title">Payment Details</span></div>
+	<?php 
+	  print $billing;
+      print $customer;
+      print $payment;
+      print $form_build_id;
+      print $form_token;
+      print $form_id;
+      print $submit;
     ?>
 	</div>
 	<div class="cartPane">
 	<?php 
+	$cid = uc_cart_get_id($create = TRUE);
+	$count  = count_cart_items($cid);
+	$subtotal = cart_total_amount($cid);
 	
-        $cart = uc_cart_get_contents();
-        
-        
-	$num 		  = count($cart);
-	$count 		  = 1;
-	$total 		  = 0;
-	$photographer = array();
-	$cart_content = "";
-	$total = 0;
-	foreach($cart as $product => $value){
-           
-                $qty[] 		= $value->qty;
-		$price          = $value->qty*$value->price;
-		
-		$photographer[] = $value->uid;
-		$photo[] 		= $value->uc_product_image['und'][0];
-		$total += $price;
-		$count+=1;
-	}
-	$p_count        = count(array_unique($photographer));
-	if($num ==1){
-	 $phototext = "photo";
-	 $photoowner = "Photographer";
-	}
-	else if($p_count==1){
-	 $photoowner = "Photographer";
-	 $phototext = "photos";
-	}
-	else{
-	 $photoowner = "Photographers";
-	 $phototext = "photos";
-	}
        ?>
 	   <div class="cartSummary">
 			<div class="blueHeading">Order Summary</div>
-			<div class="boldText"><?php echo $num." ".$phototext;?> by <?php echo $p_count." ".$photoowner;?></div>
-			<div class="totalPrice">Total Price: <div class="currencyText">&euro;<?php echo $total;?></div></div>
+			<div class="order-items">Photos: <span id="order-items"><?php echo $count ?></span></div>
+			<div class="total-price">Total Price: <span class="order-currency"><?php echo variable_get('uc_currency_sign') ?></span>
+			<span id ='order-subtotal'><?php echo $subtotal ?></span>
+			</div>
 		</div>
-		<table><tr>
-		<?php
-	for($i=1;$i<$count;$i++){
-           
-       	
-		?>
-		
-		
-		<td><?php echo theme("image_formatter", array('item' => $photo[$i-1], 'image_style' => 'search_thumbnail'));?></td>
-		<?php
-		if($i%3==0){?>
-			</tr><tr>
-			<?php }
-			?>
-		
-		<?php
-		
-       
-		  
-		  }
-	 
-	  
-	 ?>
-	 </tr></table>
+		<div class = 'thumbnails'>
+		  <?php 
+		  $i = 0;    
+		  foreach (uc_cart_get_contents($cid) as $index => $product) {
+              $photo = $product->uc_product_image['und'][0];
+              $nid = $product->nid;
+              $i++;
+		    ?>
+		 	<div id = "thumb-<?php echo $nid ?>" class="image-thumbnail">
+  		      <div id="<?php echo $nid ?>" class="remove-cart-item" >x</div>  
+		      <?php echo theme("image_formatter", array('item' => $photo, 'image_style' => 'search_thumbnail'));?>
+		  	</div>
+		   <?php } ?>
+		</div>
 	</div>
-  
-
 </div>
