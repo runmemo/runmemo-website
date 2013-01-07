878cde93f1b633b1beb96fa38b2adacbfa6f7d0c
byapparov <byapparov@gmail.com>
2013-01-07 19:29:22 +0000

    Fixed product node page.

--- sites/all/modules/custom/bib_number/bib_number.js
+++ sites/all/modules/custom/bib_number/bib_number.js
@@ -5,62 +5,7 @@
 	Drupal.behaviors.runmemoBibNumbersField = {
 		attach : function(context, settings) {
 
-	function onAddTag(elem) {
-	  var base_path = Drupal.settings.basePath;
-	  var nid = $('.node-type-product #product_id').val();
-	
-	  //define php info and make ajax call
-	  $.ajax({
-	      url : base_path + "ajax/save_numbers",
-	      type: "POST",
-	      data: {nid: nid, number: elem, option: "add"},
-              dataType: 'json',
-	      cache: false,
-	      success : function(msg) {
-                if (msg.success) {
-                  $( ".node-type-product #product-nice-message-container" ).text("Number " + elem + " was added");
-                } else {
-	          $('.tag').each(function() {
-	            if($(this).children('span').text() == elem) {
-                      $(this).css('background-color', 'yellow'); 
-                    }
-                  });
-                  $( ".node-type-product #product-nice-message-container" ).text(elem + " cannot be added");
-                }
-	      }
-	  });
-	}
-	
-	//
-	function onRemoveTag(elem) {
-	  var base_path = Drupal.settings.basePath;
-	  var nid = $('.node-type-product #product_id').val();
-	
-	  //define php info and make ajax call
-	  $.ajax({
-	      url : base_path + "ajax/save_numbers",
-	      type: "POST",
-	      data: {nid: nid, number: elem, option: "remove"},
-              dataType: 'json',              
-	      cache: false,
-	      success : function(msg) {
-                if (msg.success) {
-                  $( ".node-type-product #product-nice-message-container" ).text("Number " + elem + " was  removed");
-                } else {
 
-                }
-	      }
-	  });
-	}
-	
-	// @todo: implement some validation
-	function onChangeTag(elem_tags, elem) {
-	//    $('.tag').each(function() {
-	//            if(IsValidAge($(this).text()))
-	//                    $(this).css('background-color', 'yellow');
-	//    });
-	}
-	
     $('#tags-input').tagsInput({
 	  //   'autocomplete_url': url_to_autocomplete_api,
 	  //   'autocomplete': { option: value, option: value},
@@ -76,6 +21,66 @@
 	         'maxChars' : 6 //if not provided there is no limit,
 	  //   'placeholderColor' : '#666666'
 	  });
+
+	function onAddTag(elem) {
+	  
+	  //define php info and make ajax call
+	  $.ajax({
+	      url : settings.basePath + "ajax/save_numbers",
+	      type: "POST",
+	      data: {nid: settings.product.nid, number: elem, option: "add"},
+        dataType: 'json',
+	      cache: false,
+	      success : function(msg) {
+                if (msg.success) {
+                  $( ".node-type-product #product-nice-message-container" ).text("Number " + elem + " was added");
+                } else {
+                  $('.tag').each(function() {
+                  if($(this).children('span').text() == elem) {
+                      $(this).css('background-color', 'yellow'); 
+                    }
+                  });
+                  $( ".node-type-product #product-nice-message-container" ).text(elem + " cannot be added");
+                }
+	      },
+        error : function(msg) {
+          console.log(msg);
+        }
+	  });
+	}
+	
+	//
+	function onRemoveTag(elem) {
+	 
+   //define php info and make ajax call
+	  $.ajax({
+	      url : settings.basePath + "ajax/save_numbers",
+	      type: "POST",
+	      data: {nid: settings.product.nid, number: elem, option: "remove"},
+              dataType: 'json',              
+	      cache: false,
+	      success : function(msg) {
+                if (msg.success) {
+                  $( ".node-type-product #product-nice-message-container" ).text("Number " + elem + " was  removed");
+                } else {
+
+                }
+	      }, 
+        error: function(msg) {
+          console.log(msg);
+        }
+	  });
+	}
+	
+	// @todo: implement some validation
+	function onChangeTag(elem_tags, elem) {
+	//    $('.tag').each(function() {
+	//            if(IsValidAge($(this).text()))
+	//                    $(this).css('background-color', 'yellow');
+	//    });
+	}
+	
+  
     }
   }
 })(jQuery);
